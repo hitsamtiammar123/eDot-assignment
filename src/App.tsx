37b77cce782a66d5743def0ee145cde6c126316c
Hitsam Tiammar <hitsamtiammmar@gmail.com>
2023-02-14 16:57:58 +0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Container, Col, Row, Form } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
+interface InputNumber {
+  value: number;
+  checked: boolean;
+}
+
+enum TypeValue {
+  NUMBER,
+  CHECKED
+}
+
 function App() {
+  const [inputsNumber, setInputNumbers] = useState<Array<InputNumber>>([
+    {
+      value: 0,
+      checked: false,
+    },
+    {
+      value: 0,
+      checked: false,
+    },
+    {
+      value: 0,
+      checked: false,
+    }
+  ])
+
+  const [result, setResult] = useState<number | null>(null);
+
+  function onValueChange(index: number, value: string | boolean, type: TypeValue){
+    const newData = [...inputsNumber];
+    if(type === TypeValue.NUMBER){
+      newData[index] = {
+        ...newData[index],
+        value: Number(value)
+      }
+    }else if(type === TypeValue.CHECKED){
+      newData[index] = {
+        ...newData[index],
+        checked: value as boolean
+      }
+    }
+    setInputNumbers(newData);
+  }
+
+  function onSubmit(operation: string){
+    const checkInput = inputsNumber[0].checked;
+    if(!checkInput){
+      alert('Please check at least two checkboxes');
+      return;
+    }
+    const newResult = inputsNumber.slice(1).reduce((prev: number, curr: InputNumber) => {
+      if(!curr.checked){
+        return prev;
+      }
+      switch(operation){
+        case '+':
+          return prev + curr.value;
+        case '*':
+          return prev * curr.value;
+        case '/':
+          return prev / curr.value;
+        case '-':
+          return prev - curr.value
+      }
+      return prev;
+    }, inputsNumber[0].value)
+    setResult(newResult);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="App my-5">
+      {inputsNumber.map((input, index) => (
+        <Row key={index}>
+          <Col>
+            <Form.Control onChange={(e) => onValueChange(index, e.target.value, TypeValue.NUMBER)} value={input.value} type="number" />
+          </Col>
+          <Col>
+            <Form.Check onChange={() => onValueChange(index, !input.checked, TypeValue.CHECKED)} checked={input.checked} type="checkbox" />
+          </Col>
+        </Row>
+      ))}
+      <div className="d-flex flex-row mt-4">
+        <Button onClick={() => onSubmit('+')} className="me-3">+</Button>
+        <Button onClick={() => onSubmit('-')} className="me-3">-</Button>
+        <Button onClick={() => onSubmit('*')} className="me-3">*</Button>
+        <Button onClick={() => onSubmit('/')} className="me-3">/</Button>
+      </div>
+      <div className="d-flex flex-row mt-4">
+        <span>Hasil: {result}</span>
+      </div>
+    </Container>
   );
 }
 
